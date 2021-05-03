ca71b800dfec6a5178b51379817fb7c4cb514c2e
Paulo Oliveira Siqueira Junior <55473849+PauloJunior65@users.noreply.github.com>
2021-05-03 16:46:27 -0400

    Update get_error.m

--- barco - auto/get_error.m
+++ barco - auto/get_error.m
@@ -5,26 +5,29 @@ error.rmse = [];
 error.nrmse = [];
 error.mape = [];
 error.regression = [];
+error.x = [];
 error.output(1).mse = [];
 error.output(1).rmse = [];
 error.output(1).nrmse = [];
 error.output(1).mape = [];
 error.output(1).regression = [];
+error.output(1).x = [];
 
 if ~isempty(output) && ~isempty(target)
     error.mse = mse(target,output);
     error.rmse=sqrt(error.mse);
-    error.nrmse=error.rmse./(max(target)-min(target));
-    error.mape=mean(abs(output-target)./target,2)*100;
+    error.x = sum(target(:)/(size(target,1)*size(target,2)));
+    error.nrmse=error.rmse/error.x;
+    error.mape=mean(abs(target-output)./target,2);
     error.regression=regression(target,output);
     for i = 1:size(target,1)
         dado.mse = mse(target(i,:),output(i,:));
         dado.rmse = sqrt(dado.mse);
-        dado.nrmse = dado.rmse/(max(target(i,:))-min(target(i,:)));
-        dado.mape = mean(abs(output(i,:)-target(i,:))./target(i,:))*100;
+        dado.x = sum(target(i,:)/size(target,1));
+        dado.nrmse = dado.rmse/dado.x;
+        dado.mape = mean(abs(target(i,:)-output(i,:))./target(i,:));
         dado.regression = regression(target(i,:),output(i,:));
         error.output(i) = dado;
     end
 end
-
 end
